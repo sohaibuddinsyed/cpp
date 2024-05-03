00b516f331897a4ff4e7e3d4e3be85ee04d08107
Sohaib Uddin Syed <coolsohaib602@gmail.com>
2024-05-03 01:38:08 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,18 @@
+# Compiler settings
+CXX = g++
+CXXFLAGS = -std=c++2a
+
+# Find all .cpp files in the current directory
+SOURCES := $(wildcard *.cpp)
+
+# Generate corresponding .out file names
+TARGETS := $(SOURCES:.cpp=.out)
+
+# Rule to compile .cpp files to .out files
+all: $(TARGETS)
+
+%.out: %.cpp
+	$(CXX) $(CXXFLAGS) $< -o $@
+
+clean:
+	rm -f $(TARGETS)
---
+++ funcPtrs_lambda.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <functional>
+
+int myFunc(int a) {
+    return a + 1;
+}
+
+int main() {
+    int base = 1;
+
+    // C style functin pointer
+    int (*funcPtr)(int) = myFunc;
+    std::cout << funcPtr(10) << std::endl;
+
+    // Lambda
+    std::function<int(int)> myLambda = [&](int a) {
+        base++;
+        return base + a;
+    };
+    std::cout << myLambda(10) << std::endl;
+}
